9ebbbe8f33a40583d445684ecd22a60e3b4086f9
Brad Mcdaniel <bmcdaniel@nsmb208-xx.csudh.edu>
2018-03-19 17:19:27 -0700

    rpg.c

--- rpg.c
+++ rpg.c
@@ -5,12 +5,35 @@
 #include<stdio.h>
 
 int main(void){
+char choice;
 
 puts("welcome to room13 there please chose a direction to move  w for west e for east s for south n for north");
+scanf("%c",choice);
+if(choice=='n'){
+puts("you fell into a pit of snakes game over");
+}
+
+else if(choice=='s'){
+puts("you move into room with a lake");
+
+}
 
 
+else if(choice=='e'){
+puts("you get audited you are now poor game over");
+}
+
+
+else if(choice=='w'){
+puts(" you find the sword of truth");
+
+}
+else{
+puts("wrong button or impossible direction try again");
+}
 return 0;
 
 
 
 }
+
