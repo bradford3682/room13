d1e1df1d97c392b0fd2a1341198e192d8cbf672d
Brad Mcdaniel <bmcdaniel@nsmb208-xx.csudh.edu>
2018-03-19 17:04:05 -0700

    rpg.c

--- rpg.c
+++ rpg.c
@@ -1,1 +1,16 @@
+//Bradford Mcdaniel
+//csc 251
+//lab14
+#include<stdlib.h>
+#include<stdio.h>
 
+int main(void){
+
+puts("welcome to room13 there please chose a direction to move  w for west e for east s for south n for north");
+
+
+return 0;
+
+
+
+}
